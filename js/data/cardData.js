20d156224b5a9ef932520e591baf3f16f19780fe
TyreseCardinal <tyrese.k.cardinal@shaw.ca>
2026-08-14 02:05:57 -0600

    initial commit

---
+++ js/data/cardData.js
@@ -0,0 +1,18 @@
+const SUITS = ["hearts", "diamonds", "clubs", "spades"];
+
+const RANKS = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "jack",
+  "queen",
+  "king",
+  "ace",
+  "joker",
+];
